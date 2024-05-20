87cffe4ca7abdd2ad731a092e6a9ced5a98ce462
Salim khadir <63880607+SalimKH5@users.noreply.github.com>
2024-05-20 21:54:47 +0200

    unoptimized

--- src/app/components/PostPicture.tsx
+++ src/app/components/PostPicture.tsx
@@ -5,7 +5,7 @@ const PostPicture = ({src}:{src:string}) => {
     <div className='w-full h-full min-h-[30rem] relative  '>
             <Image src={src} 
             alt={src} priority  fill 
-            
+            unoptimized
             className="object-cover   w-full absolute left-0 right-0 h-full rounded-lg"/>
     </div>
   )
